448f4b4af9b3941486745a18bf1d3bd49f86bacd
Antoine <antoine@streem.com.au>
2018-02-25 22:11:19 +1100

    tidy up code & added cleanup task

--- lib/mina/foreman/tasks.rb
+++ lib/mina/foreman/tasks.rb
@@ -1,10 +1,16 @@
-set :foreman_app, -> { "#{fetch(:application_name)}" }
+set :foreman_app, -> { fetch(:application_name) }
 set :foreman_user, -> { fetch(:user) }
 set :foreman_log,  -> { "#{fetch(:shared_path)}/log" }
-set :foreman_sudo, true
-set :foreman_format, 'systemd'
-set :foreman_location, '/etc/init'
 
+set :foreman_format, 'systemd'
+set :foreman_location, -> {
+  case fetch(:foreman_format)
+  when 'systemd'
+    "/etc/systemd/system"
+  else
+    "/etc/init"
+  end
+}
 set :foreman_service, -> {
   case fetch(:foreman_format)
   when 'systemd'
@@ -16,59 +22,61 @@ set :foreman_service, -> {
 set :foreman_procfile, 'Procfile'
 
 namespace :foreman do
-  
-  desc 'Export the Procfile to Ubuntu systemd scripts'
-  task :export do
-    sudo_cmd = "sudo" if fetch(:foreman_sudo)
-    export_cmd = "#{sudo_cmd} bundle exec foreman export #{fetch(:foreman_format)} #{fetch(:foreman_location)} -a #{fetch(:foreman_app)} -u #{fetch(:foreman_user)} -d #{fetch(:current_path)} -l #{fetch(:foreman_log)} -f #{fetch(:foreman_procfile)}"
 
-    command %{
-      echo "-----> Exporting foreman procfile for #{fetch(:foreman_app)}"
-      #{echo_cmd %[cd #{fetch(:current_path)} ; #{export_cmd}]}
-    }
+  desc 'Export the Procfile to init scripts'
+  task :export do
+
+    comment "-----> Exporting foreman procfile for #{fetch(:foreman_app)}"
+    in_path "#{fetch(:current_path)}" do
+      command %{ sudo bundle exec foreman export #{fetch(:foreman_format)} #{fetch(:foreman_location)} -a #{fetch(:foreman_app)} -u #{fetch(:foreman_user)} -d #{fetch(:current_path)} -l #{fetch(:foreman_log)} -f #{fetch(:foreman_procfile)} }
+    end
 
     if fetch(:foreman_format) == 'systemd'
-      command %{
-        echo "-----> Enabling & reloading SystemD units for #{fetch(:foreman_app)}"
-        #{echo_cmd %[#{sudo_cmd} systemctl enable #{fetch(:foreman_service)}]}
-        #{echo_cmd %[#{sudo_cmd} systemctl daemon-reload]}
-      }
+      comment "-----> Reloading and Enabling SystemD units for #{fetch(:foreman_app)}"
+      command %{ sudo systemctl daemon-reload }
+      command %{ sudo systemctl enable #{fetch(:foreman_app)}.target }
     end
   end
 
   desc "Start the application services"
   task :start do
-    command %{echo "-----> Starting #{fetch(:foreman_app)} services"}
+    comment "-----> Starting #{fetch(:foreman_app)} services"
 
-    command echo_cmd case fetch(:foreman_format)
-                     when 'systemd'
-                       %[sudo systemctl start #{fetch(:foreman_service)}]
-                     else
-                       %[sudo start #{fetch(:foreman_service)}]
-                     end
+    case fetch(:foreman_format)
+    when 'systemd'
+      command %[sudo systemctl start #{fetch(:foreman_service)}]
+    else
+      command %[sudo start #{fetch(:foreman_service)}]
+    end
   end
 
   desc "Stop the application services"
   task :stop do
-    command %{echo "-----> Stopping #{fetch(:foreman_app)} services"}
+    comment "-----> Stopping #{fetch(:foreman_app)} services"
 
-    command echo_cmd case fetch(:foreman_format)
-                     when 'systemd'
-                       %[sudo systemctl stop #{fetch(:foreman_service)}]
-                     else
-                       %[sudo stop #{fetch(:foreman_service)}]
-                     end
+    case fetch(:foreman_format)
+    when 'systemd'
+      command %[sudo systemctl stop #{fetch(:foreman_service)}]
+    else
+      command %[sudo stop #{fetch(:foreman_service)}]
+    end
   end
 
   desc "Restart the application services"
   task :restart do
-    command %{echo "-----> Restarting #{fetch(:foreman_app)} services"}
+    comment "-----> Restarting #{fetch(:foreman_app)} services"
 
-    command echo_cmd case fetch(:foreman_format)
-                     when 'systemd'
-                       %[sudo systemctl restart #{fetch(:foreman_service)}]
-                     else
-                       %[sudo start #{fetch(:foreman_service)} || sudo restart #{fetch(:foreman_service)}]
-                     end
+    case fetch(:foreman_format)
+    when 'systemd'
+      command %[sudo systemctl restart #{fetch(:foreman_service)}]
+    else
+      command %[sudo start #{fetch(:foreman_service)} || sudo restart #{fetch(:foreman_service)}]
+    end
+  end
+
+  desc "Delete current init files"
+  task :cleanup do
+    comment "-----> Cleaning-up #{fetch(:foreman_app)} services"
+    command %[sudo rm -r #{fetch(:foreman_location)}/#{fetch(:foreman_app)}*]
   end
 end
